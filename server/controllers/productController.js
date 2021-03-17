44d0dca87daa070a087a8d61615c1c7c3138c4f3
Sanyam Suthar <sanyams0007@gmail.com>
2021-03-17 16:59:46 +0530

    added search, filter and pagination to the API

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,6 +2,8 @@
 const Product = require("../models/product");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
+
 // @route       POST api/admin/products/new
 // @description  creates a new product
 // @access
@@ -17,7 +19,16 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 // @description  gets all products
 // @access
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const resultsPerPage = 4;
+  const productCount = await Product.countDocument();
+
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultsPerPage);
+
+  const products = await apiFeatures.query;
+
   res.status(200).json({
     success: true,
     count: products.length,
